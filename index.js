52c31bbe8b8184d9aeb797fe53f3f8d5d66951ed
DenisMalv <kissed03@gmail.com>
2021-11-24 21:51:09 +0200

    milioner

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 // import "./js/lesson-1/task2.js"
 // import "./js/lesson-1/task3.js"
 // import "./js/lesson-1/task3anna.js"
-// import "./js/lesson-1/task4.js"
+import "./js/lesson-1/task4.js"
 // import "./js/lesson-2/task-1.js"
 // import "./js/lesson-3/task-1.js"
 // import "./js/lesson-4/task-1.js"
 // import "./js/lesson-4/task-1-copy.js"
 
 // import "./js/shpora/function.js"
-import "./js/shpora/object.js"
+// import "./js/shpora/object.js"
 
